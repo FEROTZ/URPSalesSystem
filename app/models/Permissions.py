421708e13b8eebaf8c9e69c15cc6cd3b71242c38
Fernando Cortés <fernando.cortes971@gmail.com>
2024-09-30 16:00:19 -0600

    create cruds to Permission model

--- app/models/Permissions.py
+++ app/models/Permissions.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Enum, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Enum, DateTime, exc
+from sqlalchemy.orm import relationship, Session
 from datetime import datetime
 from ..database import Base
 
@@ -14,3 +14,63 @@ class Permission(Base):
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
     roles = relationship('Role', secondary='roles_permissions', back_populates='permissions')
+
+    def save(db: Session, **kwargs):
+        try:
+            permission = Permission(**kwargs)
+            db.add(permission)
+            db.commit()
+            db.refresh(permission)
+            return permission
+        except Exception as error:
+            print(error)
+            return {}
+
+    def find(db: Session):
+        try:
+            return db.query(Permission).filter_by(status = 'active').all()
+        except Exception as error:
+            print(error)
+            return {}
+
+    def find_one(db: Session, **kwargs):
+        try:
+            return db.query(Permission).filter_by(**kwargs).first()
+        except Exception as error:
+            print(error)
+            return {}
+
+    def update(db: Session, permission_id: int, **body):
+        try:
+            update_permission = (
+                db.query(Permission)
+                .filter_by(
+                    id = permission_id,
+                    status = 'active'
+                )
+                .update(body, synchronize_session="fetch")
+            )
+            db.commit()
+
+            return update_permission
+        except Exception as error:
+            print(error)
+            db.rollback()
+            return {}
+
+    def delete(db: Session, **kwargs):
+        try:
+            delete_permission = (
+                db.query(Permission)
+                .filter_by(**kwargs)
+                .update({
+                    "status": "inactive",
+                    "updated_at": datetime.now(),
+                }, synchronize_session="fetch")
+            )
+            db.commit()
+            return delete_permission
+        except exc.SQLAlchemyError as error:
+            print(error)
+            db.rollback()
+            return {}
