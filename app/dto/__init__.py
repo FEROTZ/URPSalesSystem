100be520b962629a781121276aae446d383db742
Fernando Cortés <fernando.cortes971@gmail.com>
2024-09-29 23:29:20 -0600

    import duplicated fixed

--- app/dto/__init__.py
+++ app/dto/__init__.py
@@ -1,5 +1,4 @@
 from .input.create_user_input_dto import *
-from .input.create_user_input_dto import *
 from .output.get_user_output_dto import *
 from .output.user_schema_output_dto import *
 from .output.create_user_output_dto import *
