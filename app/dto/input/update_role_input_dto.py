49b9d7e0e82e5d9d461768f3b7bcea39b2aa1626
Fernando Cortés <fernando.cortes971@gmail.com>
2024-09-30 18:19:13 -0600

    change requirements to update role

--- app/dto/input/update_role_input_dto.py
+++ app/dto/input/update_role_input_dto.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 
 class UpdateRoleInputSchema(BaseModel):
-    name: Optional[str] = Field(None, description="Role name")
-    description: Optional[str] = Field(None, description="Role description")
+    name: Optional[str] = Field(None, min_length=5, max_length=50, description="Role name")
+    description: Optional[str] = Field(None, min_length=5, max_length=100,  description="Role description")
 
     class Config:
         orm_mode = True
